afc9fab9746c5b1e755e49cd47b4afff21c823b8
nsltharaka <nsltharaka@gmail.com>
2024-05-22 19:05:44 +0530

    fetching posts with pagination

--- app/(tab)/index.jsx
+++ app/(tab)/index.jsx
@@ -1,52 +1,69 @@
 import { useRouter } from 'expo-router'
 import React, { useEffect, useState } from 'react'
-import { RefreshControl, ScrollView, Text, View } from 'react-native'
+import { ActivityIndicator, Alert, FlatList, RefreshControl, Text, View } from 'react-native'
 import EmptyScreen from '../../components/EmptyScreen'
-import { useAuth } from '../../context/authContext'
-import { refreshHome } from '../../lib/api'
+import { getPosts, refreshHome } from '../../lib/api'
 
 export default function HomePage() {
 
-  const { user } = useAuth()
+  const limit = 10
+
   const router = useRouter()
 
   const [posts, setPosts] = useState([])
+  const [nextPage, setNextPage] = useState(`/posts/all?limit=${limit}&offset=${0}`)
+  const [loading, setLoading] = useState(false)
   const [refreshing, setRefreshing] = useState(false)
 
-  useEffect(
-    () => {
-      console.log("HOMEPAGE: fetching posts...")
-      setPosts([])
+  const fetchPosts = async () => {
+    if (loading) return
 
-    }, [])
+    console.log("fetching posts...");
+    try {
+      setLoading(true)
+      const data = await getPosts(nextPage)
+      console.log("fetched ", data.info.count);
 
-  return (
-    <>
-      {
-        posts.length > 0 ?
-          <View className='flex-1 bg-green-200 '>
-            <ScrollView overScrollMode='never' showsVerticalScrollIndicator={false}
-              refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => refreshHome()} />}
-            >
-              <Text>{JSON.stringify(user, null, 2)}</Text>
-            </ScrollView>
-          </View>
-          :
-          <ScrollView overScrollMode='never' showsVerticalScrollIndicator={false}
-            refreshControl={<RefreshControl refreshing={refreshing} onRefresh={async () => {
-              setRefreshing(true)
-              await refreshHome()
-              setRefreshing(false)
-            }} />}
-          >
-            <EmptyScreen
-              infoMsg={"Add topics to see latest updates."}
-              linkText={"Add Topic"}
-              onPressHandler={() => router.push("screens/addTopic")}
-            />
-          </ScrollView>
+      if (data.posts.length === 0) {
+        return
       }
 
-    </>
+      setPosts([...posts, data.posts])
+      setNextPage(data.info.next)
+
+    } catch (error) {
+      Alert.alert("Error fetching posts", error)
+
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => { fetchPosts() }, [])
+
+  return (
+    <View className='flex-1 bg-green-200 '>
+      <FlatList
+        overScrollMode='never'
+        showsVerticalScrollIndicator={false}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => refreshHome()} />}
+
+        data={posts}
+        renderItem={({ item }) => <Text>{JSON.stringify(item, null, 2)}</Text>}
+
+        onEndReached={() => fetchPosts()}
+        onEndReachedThreshold={3}
+
+        ListFooterComponent={() => loading && <ActivityIndicator size={"large"} color={"red"} />}
+
+        ListEmptyComponent={() => (
+          <EmptyScreen
+            infoMsg={"Add topics to see latest updates."}
+            linkText={"Add Topic"}
+            onPressHandler={() => router.push("screens/addTopic")}
+          />
+        )}
+      />
+    </View>
   )
 }
